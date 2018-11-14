041a02d8897ae4a7096df236ce5795c14ea39a3a
yurait6@gmail.com <yurait6@gmail.com>
2018-11-15 01:40:54 +0300

    Obfuscation example

--- hw1/src/main/java/ru/otus/java/Work1.java
+++ hw1/src/main/java/ru/otus/java/Work1.java
@@ -5,6 +5,18 @@ package ru.otus.java;
  */
 public class Work1 {
 
+    //Не получается обфусцировать с dependency log4j. Куча ошибок вида
+    /**
+     *  [proguard] Unexpected error while evaluating instruction:
+     *  [proguard]   Class       = [org/apache/log4j/LogManager]
+     *  [proguard]   Method      = [<clinit>()V]
+     *
+     *  Пробовал добавлять -keep с ключами по всем методам класса и по тем, которые указаны в трейсе,
+     *  но не помогло(
+     */
+
+//    private static final Logger LOG = Logger.getLogger(Work1.class);
+
     public static void main(String[] args)  {
         System.out.println("Hello world!");
 
