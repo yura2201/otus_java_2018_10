b5008d4204cdf4bfb2d864ade84ac615c58dba8a
Yuriy Tsarkov <Y.Tsarkov@crif.com>
2018-11-15 19:33:50 +0300

    organized imports

--- hw1/src/main/java/ru/otus/java/Work1.java
+++ hw1/src/main/java/ru/otus/java/Work1.java
@@ -9,7 +9,6 @@ import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
